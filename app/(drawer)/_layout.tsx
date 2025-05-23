3991a83b5277d5e06f79a346c9812b50ac584f3f
David Gómez <davegomez426@gmail.com>
2025-05-23 19:41:27 -0300

    Correccion de archivos

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,13 +1,26 @@
+import CustomDrawer from "@/components/shared/CustomDrawer";
 import { Drawer } from "expo-router/drawer";
 import React from "react";
 
 const DrawerLayout = () => {
   return (
     <Drawer
+      drawerContent={CustomDrawer}
       screenOptions={{
         overlayColor: "rgba(0, 0, 0, 0.5)",
+        drawerActiveTintColor: "indigo",
+        sceneStyle: {
+          backgroundColor: "white",
+        },
       }}
     >
+      <Drawer.Screen
+        name="tabs"
+        options={{
+          title: "Horario",
+          drawerLabel: "Horario",
+        }}
+      />
       <Drawer.Screen
         name="user/index"
         options={{
